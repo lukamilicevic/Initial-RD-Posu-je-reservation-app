e2656ee37fbe71d6af4883c520109ad599e7b3f4
lukamilicevic <lukamilicevic6@gmail.com>
2026-08-07 17:05:19 +0200

    Add 'SUPABASE_' to envPrefix in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
+  envPrefix: ['VITE_', 'NEXT_PUBLIC_', 'SUPABASE_'],
   plugins: [react()]
 });
